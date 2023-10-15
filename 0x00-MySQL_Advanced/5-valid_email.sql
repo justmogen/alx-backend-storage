5d8e4d9989ce7be936d6188503c73bc928e7957c
justmogen <justmogen@gmail.com>
2023-10-15 17:23:37 +0300

    email validation check

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+--trigger to update email validation
+DELIMITER //
+
+CREATE TRIGGER update_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END
+//
+
+DELIMITER ;
