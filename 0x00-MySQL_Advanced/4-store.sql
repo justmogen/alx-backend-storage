2b52803db774782acd392ac1689890259a586a94
justmogen <justmogen@gmail.com>
2023-10-15 15:59:43 +0300

    syntax fixed

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,26 @@
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    DECLARE current_quantity INT;
+    DECLARE item_name VARCHAR(255);
+    
+    -- Get the current quantity of the item
+    SELECT quantity INTO current_quantity
+    FROM items
+    WHERE name = NEW.item_name;
+    
+    -- Get the item name
+    SET item_name = NEW.item_name;
+    
+    -- Update the quantity in the items table
+    UPDATE items
+    SET quantity = current_quantity - NEW.number
+    WHERE name = item_name;
+END;
+//
+
+DELIMITER ;
+
