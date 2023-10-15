699de1bac2dfa07ee56ba358c727def5db82efc7
justmogen <justmogen@gmail.com>
2023-10-15 17:27:42 +0300

    email validation check

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,23 +1,14 @@
---trigger for email
+--trigger to update email validation
 DELIMITER //
 
-CREATE TRIGGER update_valid_email 
+CREATE TRIGGER update_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    DECLARE is_valid INT;
-    
-    -- Check if the new email is valid (you may need to adjust this condition)
-    SET is_valid = (SELECT 1 FROM DUAL WHERE NEW.email LIKE '%@%');
-
-    -- Update valid_email based on the new email's validity
-    IF is_valid THEN
-        SET NEW.valid_email = 1;
-    ELSE
-        SET NEW.valid_email = 0;
-    END IF;
-END;
+        IF NEW.email != OLD.email THEN
+                SET NEW.valid_email = 0;
+        END IF;
+END
 //
 
 DELIMITER ;
-
