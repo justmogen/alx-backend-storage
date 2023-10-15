6e235c5b32f1ccc62a8e688a750f8782429e76a4
justmogen <justmogen@gmail.com>
2023-10-15 17:06:57 +0300

    syntax fixed

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,16 @@
---store updated items using trigger
-DELIMITER / / CREATE TRIGGER decrease_quantity_after_order AFTER
-INSERT
-    ON orders FOR EACH ROW
-UPDATE items
-SET
-    quantity = quantity - NEW.number
-WHERE
-    name = NEW.item_name;
+--using triggers to update a store
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity_after_order 
+AFTER INSERT ON orders 
+FOR EACH ROW 
+BEGIN
+    -- Decrease the quantity of the ordered item
+    UPDATE items 
+    SET quantity = quantity - NEW.number 
+    WHERE name = NEW.item_name;
+END;
+//
+
+DELIMITER ;
+
