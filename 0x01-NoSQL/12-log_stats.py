f32f1ab0a4a2042e1bb87c8c0e14cf765ca5f9b9
justmogen <justmogen@gmail.com>
2023-10-23 00:21:15 +0300

    log stat

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Log stats"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb:// localhost:27017/')
+    logs_collection = client.logs.nginx
+    num_docs = logs_collection.count_documents({})
+    print(f'{num_docs} logs')
+    print('Methods:')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_method = logs_collection.count_documents({'method': method})
+        print(f'\tmethod {method}: {num_method}')
+    filter_path = {"method": "GET", "path": "/status"}
+    num_path = logs_collection.count_documents(filter_path)
+    print(f'{num_path} status check')
